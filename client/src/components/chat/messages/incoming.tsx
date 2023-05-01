23759d509f1dbd1c08750613f0c5e1fbdedd3e82
Viacheslav Iskenderov <vyacheslav.iskenderov@binary-studio.com>
2023-05-01 23:19:44 +0300

    vi-fix: fixed incoming message style

--- client/src/components/chat/messages/incoming.tsx
+++ client/src/components/chat/messages/incoming.tsx
@@ -4,7 +4,7 @@ type Props = {
 };
 
 const IncomingMessage = ({ message, username }: Props) => (
-  <div className="bg-gray-100 px-6 max-w-[83%] py-3 rounded-md flex flex-col gap-2">
+  <div className="bg-gray-100 px-6 max-w-[83%] py-3 rounded-md flex flex-col gap-2 text-sm flex-shrink self-start">
     <h5 className="text-xs text-gray-500 font-bold tracking-wider">
       {username}
     </h5>
